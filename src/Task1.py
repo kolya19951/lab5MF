4dddd3f6d059fcbada8113d6918aa4a3410859b9
Микола <kolya.simotyuk@gmail.com>
2017-11-29 18:07:09 +0200

    final

--- src/Task1.py
+++ src/Task1.py
@@ -51,58 +51,86 @@ def tma(a, b, c, d):
     return x
 
 
+# #1
 def getAnalitic():
     global N1, N2, M
-    y = np.zeros((M, N1, N2))
+    y = np.zeros((M, N2, N1))
     for t in range(M):
-        for m in range(N1):
-            for n in range(N2):
-                y[t][m][n] = exp(xi[m] + yi[n] + 2 * ti[t])
+        for m in range(N2):
+            for n in range(N1):
+                y[t][m][n] = exp(xi[n] + yi[m] + 2 * ti[t])
     return y
 
-#1
-# def getG(x, y, t):
-#     if type(t) is int:
-#         return exp(xi[x] + yi[y] + 2 * ti[t])
-#     if type(t) is float:
-#         return exp(xi[x] + yi[y] + 2 * t)
-#
-# #2
-# def getG(x, y, t):
-#     if type(t) is int:
-#         return exp(xi[x] + yi[y] + 2 * ti[t])
-#     if type(t) is float:
-#         return exp(xi[x] + yi[y] + 2 * t)
 
-#2
+# 2
+# def getAnalitic():
+#     global N1, N2, M
+#     y = np.zeros((M, N2, N1))
+#     for t in range(M):
+#         for m in range(N2):
+#             for n in range(N1):
+#                 y[t][m][n] = cos(xi[n]) * sinh(yi[m]) * exp(-4 * ti[t])
+#     return y
+
+# 3
+# def getAnalitic():
+#     global N1, N2, M
+#     y = np.zeros((M, N2, N1))
+#     for t in range(M):
+#         for m in range(N2):
+#             for n in range(N1):
+#                 y[t][m][n] = cos(xi[n] + pi / 2) * cosh(2 * yi[m]) * exp(3 * ti[t])
+#     return y
+
+
+# 1
 def getG(x, y, t):
     if type(t) is int:
-        return cos(xi[x] + pi/2)
+        return exp(xi[x] + yi[y] + 2 * ti[t])
     if type(t) is float:
         return exp(xi[x] + yi[y] + 2 * t)
+#
+# #3
+# def getG(x, y, t):
+#     if type(t) is int:
+#         return cos(xi[x] + pi / 2) * cosh(2 * yi[y]) * exp(3 * ti[t])
+#     if type(t) is float:
+#         return cos(xi[x] + pi / 2) * cosh(2 * yi[y]) * exp(3 * t)
+
+
+# 2
+# def getG(x, y, t):
+#     if type(t) is int:
+#         return cos(xi[x]) * sinh(yi[y]) * exp(-4 * ti[t])
+#     if type(t) is float:
+#         return cos(xi[x]) * sinh(yi[y]) * exp(-4 * t)
 
 
 def getTask1Result():
     global N1, N2, M
-    y = np.zeros((M, N1, N2))
+    y = np.zeros((M, N2, N1))
 
     for m in range(N1):
         for n in range(N2):
-            y[0][m][n] = getG(m, n, 0)
-            y[0][m][n] = getG(m, n, 0)
+            y[0][n][m] = getG(m, n, 0)
+            y[0][n][m] = getG(m, n, 0)
 
     for t in range(1, M):
         for m in range(N1):
             for n in range(N2):
-                y[t][0][n] = getG(0, n, t)
-                y[t][N1 - 1][n] = getG(N1 - 1, n, t)
-            y[t][m][0] = getG(m, 0, t)
-            y[t][m][N2 - 1] = getG(m, N2 - 1, t)
+                y[t][n][0] = getG(0, n, t)
+                y[t][n][N1 - 1] = getG(N1 - 1, n, t)
+            y[t][0][m] = getG(m, 0, t)
+            y[t][N2 - 1][m] = getG(m, N2 - 1, t)
 
-    for j in range(1, M - 1):
+    for j in range(1, M):
         for m in range(1, N1 - 1):
             for n in range(1, N2 - 1):
-                y[j][m][n] = tau * (getF(m, n, j) + getMu(m, n, j) *
+                # y[j][n][m] = tau * (getF(m, n, j) + getMu(m, n, j) *
+                #                     (((y[j - 1][n][m - 1] - 2 * y[j - 1][n][m] + y[j - 1][n][m + 1]) / (h1 ** 2)) + (
+                #                         y[j - 1][n - 1][m] - 2 * y[j - 1][n][m] + y[j - 1][n + 1][m]) / (h2 ** 2))) + \
+                #              y[j - 1][n][m]
+                y[j][m][n] = tau * (getF(n, m, j) + getMu(n, m, j) *
                                     (((y[j - 1][m - 1][n] - 2 * y[j - 1][m][n] + y[j - 1][m + 1][n]) / (h1 ** 2)) + (
                                         y[j - 1][m][n - 1] - 2 * y[j - 1][m][n] + y[j - 1][m][n + 1]) / (h2 ** 2))) + \
                              y[j - 1][m][n]
@@ -111,7 +139,8 @@ def getTask1Result():
 
 def LTScheme():
     global N1, N2, M
-    y = np.zeros((M, N1, N2))
+    y = np.zeros((M, N2, N1))
+    print(y.shape)
     for m in range(N1):
         for n in range(N2):
             y[0][n][m] = getG(m, n, 0)
@@ -127,32 +156,33 @@ def LTScheme():
 
     for j in range(M - 1):
         tj = ti[j] + tau / 2
-        YFict = np.zeros((N1, N2))
+        YFict = np.zeros((N2, N1))
         for m in range(N1):
             for n in range(N2):
                 YFict[n][0] = getG(0, n, tj)
                 YFict[n][N1 - 1] = getG(N1 - 1, n, tj)
             YFict[0][m] = getG(m, 0, tj)
             YFict[N2 - 1][m] = getG(m, N2 - 1, tj)
-        for n in range(1, N1 - 1):
+        for n in range(1, N2 - 1):
             a = np.zeros(N2 - 2)
             b = np.zeros(N2 - 2)
             c = np.zeros(N2 - 2)
             d = np.zeros(N2 - 2)
-            for m in range(1, N2 - 1):
+            for m in range(1, N1 - 1):
                 KN1 = (tau / (2 * ((h1) ** 2))) * getMu(xi[m], yi[n], tj)
                 KN2 = (tau / (2 * ((h2) ** 2))) * getMu(xi[m], yi[n], tj)
                 if m == 1:
                     b[m - 1] = 1 + (tau / ((h1) ** 2)) * getMu(m, n, tj)
                     c[m - 1] = -KN1
                     d[m - 1] = KN1 * getG(m - 1, n, tj) + (y[j][n - 1][m] + y[j][n + 1][m]) * KN2 + y[j][n][m] * (
-                    1 - 2 * KN2)
-                elif m == (N2 - 2):
+                        1 - 2 * KN2)
+                elif m == (N1 - 2):
                     a[m - 1] = -KN1
+                    c[len(c) - 2] = -KN2
                     b[m - 1] = 1 + (tau / ((h1) ** 2)) * getMu(m, n, tj)
                     d[m - 1] = KN1 * getG(m + 1, n, tj) + (y[j][n - 1][m] + y[j][n + 1][m]) * KN2 + y[j][n][m] * (
-                    1 - 2 * KN2)
-                    YFict[n][1:N1 - 1] = tma(a, b, c, d)
+                        1 - 2 * KN2)
+                    YFict[n, 1: N2 - 1] = tma(a, b, c, d)
                 else:
                     a[m - 1] = -KN1
                     b[m - 1] = 1 + (tau / (h1 ** 2)) * getMu(xi[m], yi[n], tj)
@@ -171,13 +201,13 @@ def LTScheme():
                     b[n - 1] = 1 + (tau / (h2 ** 2)) * getMu(m, n, j + 1)
                     c[n - 1] = -KN2
                     d[n - 1] = KN2 * y[j + 1][n - 1][m] + (YFict[n][m - 1] + YFict[n][m + 1]) * KN1 + YFict[n][m] * (
-                    1 - 2 * KN1)
+                        1 - 2 * KN1)
                 elif n == (N1 - 2):
                     a[n - 1] = -KN2
                     b[n - 1] = 1 + (tau / (h2 ** 2)) * getMu(m, n, j + 1)
                     d[n - 1] = KN2 * y[j + 1][n + 1][m] + (YFict[n][m - 1] + YFict[n][m + 1]) * KN1 + YFict[n][m] * (
-                    1 - 2 * KN1)
-                    y[j + 1][m][1:N1 - 1] = tma(a, b, c, d)
+                        1 - 2 * KN1)
+                    y[j + 1][1:N1 - 1, m] = tma(a, b, c, d)
                 else:
                     a[n - 1] = -KN2
                     b[n - 1] = 1 + (tau / (h2 ** 2)) * getMu(m, n, j + 1)
@@ -192,6 +222,7 @@ fig1 = pylab.figure()
 axes = fig1.gca(projection='3d')
 xi, yi = np.meshgrid(xi, yi)
 print(xi.shape)
-axes.plot_surface(xi, yi, y[ti.index(1.995)])
+axes.plot_surface(xi, yi, y[ti.index(1.995)], cmap='viridis')
+# axes.plot_surface(xi, yi, yA[ti.index(0.996)], cmap='viridis')
 print(np.linalg.norm(y - yA) / np.linalg.norm(yA))
 pylab.show()
